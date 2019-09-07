1b6e13bba6c46ef8769c23b6c9c476cf01110680
carban <murillo.carlos@correounivalle.edu.co>
2019-09-07 18:12:27 -0500

    payment module improved, percenteges, radio-buttons, etc

--- frontend/src/components/car.vue
+++ frontend/src/components/car.vue
@@ -1,4 +1,4 @@
-<template dark>
+<template light>
   <div class="text-xs-center">
     <v-bottom-sheet v-model="sheet" persistent>
       <!-- <v-list>
@@ -22,8 +22,8 @@
             </v-card-text>
           </v-card>
         </v-layout> -->
-        <template dark>
-  <v-stepper v-model="e1">
+        <template light>
+  <v-stepper dark v-model="e1">
     <v-stepper-header>
       <v-stepper-step :complete="e1 > 1" step="1">Confirm Products</v-stepper-step>
 
@@ -110,33 +110,58 @@
       </v-stepper-content>
 
       <v-stepper-content step="3">
+
         <template>
-          <v-expansion-panel>
+          <v-list>
+            <v-list-tile-title>
+              <h2>Cash</h2>
+            </v-list-tile-title>
+          <v-radio-group v-model="cashSelected" row>
+            <div @click="porcCash = 0" ><v-radio label="None">None</v-radio></div>
+            <v-radio v-for="(item, index) in payCash" :key="index" :label="item.name" :value="item.name" color="pink"></v-radio>
+          </v-radio-group>
+          <v-list-tile-title>
+            <h2>Ahorros</h2>
+          </v-list-tile-title>
+          <v-radio-group v-model="ahorrosSelected" row>
+            <div @click="porcAhorros = 0" ><v-radio label="None">None</v-radio></div>
+            <v-radio v-for="(item, index) in payAhorro" :key="index" :label="item.name" :value="item.name" color="pink"></v-radio>
+          </v-radio-group>
+          <v-list-tile-title>
+            <h2>Credit Card</h2>
+          </v-list-tile-title>
+          <v-radio-group v-model="creditSelected" row>
+            <div @click="porcCredit = 0" ><v-radio label="None">None</v-radio></div>
+            <v-radio v-for="(item, index) in payCredit" :key="index" :label="item.name" :value="item.name" color="pink"></v-radio>
+          </v-radio-group>
+
+          </v-list>
+          <!-- <v-expansion-panel>
             <v-expansion-panel-content>
               <template v-slot:header>
                 <div>Cash</div>
               </template>
-              <v-list-tile avatar>
-                <v-checkbox v-for="(item, index) in payCash" :key="index" :label="item.name" v-model="item.v" type="checkbox"></v-checkbox>
-              </v-list-tile>
+              <v-radio-group v-model="cashSelected" row>
+                <v-radio v-for="(item, index) in payCash" :key="index" :label="item.name" :value="item.name" color="pink"></v-radio>
+              </v-radio-group>
             </v-expansion-panel-content>
             <v-expansion-panel-content>
               <template v-slot:header>
                 <div>Debito Ahorros</div>
               </template>
-              <v-list-tile avatar>
-                <v-checkbox v-for="(item, index) in payAhorro" :key="index" :label="item.name" v-model="item.v" type="checkbox" required></v-checkbox>
-              </v-list-tile>
+              <v-radio-group v-model="ahorrosSelected" row>
+                <v-radio v-for="(item, index) in payAhorro" :key="index" :label="item.name" :value="item.name" color="pink"></v-radio>
+              </v-radio-group>
             </v-expansion-panel-content>
             <v-expansion-panel-content>
               <template v-slot:header>
                 <div>Credit Card</div>
               </template>
-              <v-list-tile avatar>
-                <v-checkbox v-for="(item, index) in payCredit" :key="index" :label="item.name" v-model="item.v" type="checkbox" required></v-checkbox>
-              </v-list-tile>
+              <v-radio-group v-model="creditSelected" row>
+                <v-radio v-for="(item, index) in payCredit" :key="index" :label="item.name" :value="item.name" color="pink"></v-radio>
+              </v-radio-group>
             </v-expansion-panel-content>
-          </v-expansion-panel>
+          </v-expansion-panel> -->
         </template>
 
         <v-btn color="primary" @click="makeShop()">
@@ -151,45 +176,33 @@
           <v-layout row>
             <v-flex grow pa-1 class="larrge">
               <v-card>
-                <h3>CASH</h3>
-                <!-- <v-layout row wrap> -->
-                <!-- <v-container grid-list-xs,sm,md,lg,xl> -->
-                  <v-list-tile avatar class="left-margin-sliders">
-                    <div class="apercen" v-for="(item, index) in payCash" :key="index">
-                      <v-slider v-if="item.v" v-model="item.porc" step="0.1" thumb-label="always" color="success"></v-slider>
-                    </div>
-                    <h1>{{Math.floor((porcCash / total)*100)}}%</h1>
+                <div v-if="cashSelected != null && cashSelected != 'None'">
+                  <h3>CASH</h3>
+                  <v-list-tile>
+                    <v-text-field :label="cashSelected" v-model="porcCash" type="number"></v-text-field>
+                  <v-list-tile-action>
+                    <h1>{{Math.round((porcCash / total)*1000)/10}}%</h1>
+                  </v-list-tile-action>
                   </v-list-tile>
-                <!-- </v-container> -->
-
-                <!-- </v-layout> -->
-
-                <!-- <v-list-tile>
-                <div class="apercen" v-for="(item, index) in payCash" :key="index">
-                  <v-text-field class="small-field" :label="item.name" v-if="item.v" v-model="item.porc" type="number"></v-text-field>
                 </div>
-                <v-list-tile-action>
-                  <h1>{{(porcCash / total)*100}}%</h1>
-                </v-list-tile-action>
-                </v-list-tile> -->
-                <h3>AHORROS</h3>
-                <v-list-tile>
-                <div class="apercen" v-for="(item, index) in payAhorro" :key="index">
-                  <v-text-field class="small-field" :label="item.name" v-if="item.v" v-model="item.porc" type="number"></v-text-field>
+                <div v-if="ahorrosSelected != null && ahorrosSelected != 'None'">
+                  <h3>AHORROS</h3>
+                  <v-list-tile>
+                    <v-text-field :label="ahorrosSelected" v-model="porcAhorros" type="number"></v-text-field>
+                  <v-list-tile-action>
+                    <h1>{{Math.round((porcAhorros / total)*1000)/10}}%</h1>
+                  </v-list-tile-action>
+                  </v-list-tile>
                 </div>
-                <v-list-tile-action>
-                  <h1>{{porcAhorro}}%</h1>
-                </v-list-tile-action>
-                </v-list-tile>
-                <h3>CREDIT</h3>
-                <v-list-tile>
-                <div class="apercen" v-for="(item, index) in payCredit" :key="index">
-                  <v-text-field class="small-field" :label="item.name" v-if="item.v" v-model="item.porc" type="number"></v-text-field>
+                <div v-if="creditSelected != null && creditSelected != 'None'">
+                  <h3 >CREDIT</h3>
+                  <v-list-tile>
+                    <v-text-field :label="creditSelected" v-model="porcCredit" type="number"></v-text-field>
+                  <v-list-tile-action>
+                    <h1>{{Math.round((porcCredit / total)*1000)/10}}%</h1>
+                  </v-list-tile-action>
+                  </v-list-tile>
                 </div>
-                <v-list-tile-action>
-                  <h1>{{porcCredit}}%</h1>
-                </v-list-tile-action>
-                </v-list-tile>
               </v-card>
               </v-flex>
 
@@ -197,13 +210,13 @@
                 <v-card>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                   <button @click="exportpdf()">PDF</button>
-                  <h1>${{total}}</h1>
-                  <h1>{{porTotal}}</h1>
+                  <h1>Total Price: ${{total}}</h1>
+                  <h1>Total percentage: {{porTotal}}%</h1>
                 </v-card>
               </v-flex>
 
           </v-layout>
-          <v-btn color="primary" @click="e1 = 5">
+          <v-btn color="primary" @click="verifyPercentages()">
             Continue
           </v-btn>
           <v-btn @click="e1 = 3">Back</v-btn>
@@ -224,10 +237,6 @@
     </v-stepper-items>
   </v-stepper>
 </template>
-
-        <!-- <v-btn v-if="car.length>0" color="primary">BUY</v-btn> -->
-      <!-- </v-list> -->
-
     </v-bottom-sheet>
   </div>
 </template>
@@ -235,6 +244,8 @@
 <script>
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
+
+var bbvaI = require('@/assets/bbva.png');
 
 export default {
   computed: {
@@ -244,31 +255,19 @@ export default {
     car(){
       return this.$store.getters.car;
     },
-    porcCash(){
-      let t = 0;
-      for (var i = 0; i < this.payCash.length; i++) {
-        t += parseInt(this.payCash[i].porc, 10);
-      }
-      return t;
-    },
-    porcAhorro(){
-      let t = 0;
-      for (var i = 0; i < this.payCash.length; i++) {
-        t += parseInt(this.payAhorro[i].porc, 10);
-      }
-      return t;
-    },
-    porcCredit(){
-      let t = 0;
-      for (var i = 0; i < this.payCash.length; i++) {
-        t += parseInt(this.payCredit[i].porc, 10);
-      }
-      return t;
-    },
     porTotal(){
-      let t = this.porcCash+this.porcAhorro+this.porcCredit;
-      if (t >= 0 && t <= 100){
-        return t;
+      var t = 0;
+      if (this.cashSelected!=null && this.cashSelected!='none') {
+        t += parseFloat(this.porcCash);
+      }
+      if(this.ahorrosSelected!=null && this.ahorrosSelected!='none'){
+        t += parseFloat(this.porcAhorros);
+      }
+      if(this.creditSelected!=null && this.creditSelected!='none'){
+        t += parseFloat(this.porcCredit);
+      }
+      if (t >= 0 && t <= this.total){
+        return Math.round((t*1000)/this.total)/10;
       }else{
         return "Error"
       }
@@ -281,9 +280,16 @@ export default {
       dividers: 8,
       iva: "$3.00",
       productsToBuy: [],
-      payCash: [{name: "Efecty", v:false, porc: 0}, {name: "Mercado Pago", v:false, porc: 0}, {name: "UPay", v:false, porc: 0}, {name: "Baloto", v:false, porc: 0}],
-      payAhorro: [{name: "Bancolombia", v:false, porc: 0}, {name: "Davivienda", v:false, porc: 0}, {name: "Occidenente", v:false, porc: 0}, {name: "PSE", v:false, porc: 0}],
-      payCredit: [{name: "Master Card", v:false, porc: 0}, {name: "Visa", v:false, porc: 0}, {name: "American Express", v:false, porc: 0}, {name: "Diners Club", v:false, porc: 0}],
+      payCash: [{name: "Efecty"}, {name: "Mercado Pago"}, {name: "Gane"}, {name: "Baloto"}],
+      payAhorro: [{name: "Bancolombia"}, {name: "Davivienda"}, {name: "BBVA"}, {name: "Colpatria"}],
+      payCredit: [{name: "Master Card"}, {name: "Visa"}, {name: "American Express"}, {name: "Diners Club"}],
+      cashSelected: null,
+      ahorrosSelected: null,
+      creditSelected: null,
+      porcCash: 0,
+      porcAhorros: 0,
+      porcCredit: 0,
+      bbva: bbvaI
     }
   },
   methods: {
@@ -326,6 +332,37 @@ export default {
       }
       this.total = Math.round(aux * 100) / 100;
     },
+    verifyPercentages(){
+
+      var a = true;
+      var b = true;
+      var c = true;
+
+      // Necesito validar que a los medios de pago que selecciono les de un valor mayor a 0
+      // Porque si no deberia devolverse y desactivar el medio de pago
+      //Si lo elige es porque va a pagar algo mayor a cero
+      if (this.cashSelected!=null && this.cashSelected!='none') {
+        if ((Math.round((this.porcCash / this.total)*1000)/10) == 0) {
+          a = false;
+        }
+      }
+      if(this.ahorrosSelected!=null && this.ahorrosSelected!='none'){
+        if ((Math.round((this.porcAhorros / this.total)*1000)/10) == 0) {
+          b = false;
+        }
+      }
+      if(this.creditSelected!=null && this.creditSelected!='none'){
+        if ((Math.round((this.porcCredit / this.total)*1000)/10) == 0) {
+          c = false;
+        }
+      }
+
+      if (this.porTotal == 100 && a && b && c) {
+        this.e1 = 5;
+      }else{
+        alert("Oye tranquilo viejo")
+      }
+    },
     makeShop(){
       // if (this.porTotal==100) {
       //   this.e1 = 4;
@@ -349,10 +386,10 @@ export default {
     margin-left: 21px;
   }
   .larrge{
-    width: 65%;
+    width: 50%;
   }
   .small-field{
-    width: 75%;
+    /* width: 75%; */
   }
   .left-margin-sliders{
     margin-left: 30px;
